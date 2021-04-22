a7ad7aa9793ea58f3d809aaf8696d488da99cec1
Carlos Zamora Ramírez <206990505@bancobcr.com>
2021-04-22 14:21:09 -0600

    Version Inicial

---
+++ src/main/java/com/example/helloboot/HelloBootController.java
@@ -0,0 +1,19 @@
+package com.example.helloboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kameshs
+ */
+@RestController
+public class HelloBootController {
+
+    @GetMapping("/whereami")
+    public String whereami(@Value("${message.prefix}") String prefix) {
+        String resp = String.format("%s from %s", prefix, System.getenv().getOrDefault("HOSTNAME", "localhost"));
+        return resp;
+    }
+
+}
